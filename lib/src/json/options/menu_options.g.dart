e92b4df60b701a3092bb5ead57c062bd853f732a
Chris Norman <chris.norman2@googlemail.com>
2022-02-08 13:43:50 +0000

    Regenerate json.

--- lib/src/json/options/menu_options.g.dart
+++ lib/src/json/options/menu_options.g.dart
@@ -9,10 +9,12 @@ part of 'menu_options.dart';
 MenuOptions _$MenuOptionsFromJson(Map<String, dynamic> json) => MenuOptions(
       title: json['title'] as String,
       musicId: json['musicId'] as String?,
+      fadeTime: (json['fadeTime'] as num?)?.toDouble(),
     );
 
 Map<String, dynamic> _$MenuOptionsToJson(MenuOptions instance) =>
     <String, dynamic>{
       'title': instance.title,
       'musicId': instance.musicId,
+      'fadeTime': instance.fadeTime,
     };
