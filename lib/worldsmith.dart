f78b192f7b03e1fc214ca5cead18a32971f225d6
Chris Norman <chris.norman2@googlemail.com>
2022-02-12 22:06:18 +0000

    Export the `Sound` class.

--- lib/worldsmith.dart
+++ lib/worldsmith.dart
@@ -9,6 +9,7 @@ export 'src/json/options/pause_menu_options.dart';
 export 'src/json/options/sound_options.dart';
 export 'src/json/options/world_options.dart';
 export 'src/json/reverb_preset_reference.dart';
+export 'src/json/sound.dart';
 export 'src/json/world.dart';
 export 'src/json/world_credit.dart';
 export 'src/json/zones/box.dart';
