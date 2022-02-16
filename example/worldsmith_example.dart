1ad45a2e1da59b9b204017aeac6f5155bb5548bb
Chris Norman <chris.norman2@googlemail.com>
2022-02-16 10:09:09 +0000

    Add credits to example.

--- example/worldsmith_example.dart
+++ example/worldsmith_example.dart
@@ -5,7 +5,18 @@ import 'package:worldsmith/worldsmith.dart';
 import 'package:ziggurat/ziggurat.dart';
 
 Future<void> main() {
-  final world = World(title: 'Example World');
+  final world = World(
+    title: 'Example World',
+    credits: [
+      WorldCredit(
+        id: 'ziggurat',
+        title: 'Chris Norman: Worldsmith',
+        url: 'https://pub.dev/packages/worldsmith',
+      )
+    ],
+    creditsMenuOptions: CreditsMenuOptions(fadeTime: 1.0),
+    mainMenuOptions: MainMenuOptions(fadeTime: 1.0),
+  );
   final game = Game(world.title, triggerMap: defaultTriggerMap);
   final worldContext = WorldContext(game: game, world: world);
   return runWorld(worldContext);
