943f0123612774fac47f2bfc778219e82e1a081a
Chris Norman <chris.norman2@googlemail.com>
2022-02-20 09:31:31 +0000

    Add a method to get random coordinates.

--- lib/src/json/commands/local_teleport.dart
+++ lib/src/json/commands/local_teleport.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:json_annotation/json_annotation.dart';
 
 import '../zones/coordinates.dart';
+import '../zones/zone.dart';
 
 part 'local_teleport.g.dart';
 
@@ -25,4 +28,28 @@ class LocalTeleport {
 
   /// Convert an instance to JSON.
   Map<String, dynamic> toJson() => _$LocalTeleportToJson(this);
+
+  /// Get an appropriate set of coordinates to use.
+  ///
+  /// If [maxCoordinates] is `null`, `[minCoordinates] will be returned.
+  /// Otherwise, a set of random coordinates between the two will be returned.
+  Point<int> getCoordinates({
+    required Zone zone,
+    required Random random,
+  }) {
+    final lower = zone.getAbsoluteCoordinates(minCoordinates);
+    final end = maxCoordinates;
+    if (end == null) {
+      return lower;
+    }
+    final upper = zone.getAbsoluteCoordinates(end);
+    final startX = min(lower.x, upper.x);
+    final startY = min(lower.y, upper.y);
+    final endX = max(lower.x, upper.x);
+    final endY = max(lower.y, upper.y);
+    return Point(
+      startX + random.nextInt(endX),
+      startY + random.nextInt(endY),
+    );
+  }
 }
