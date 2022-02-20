6e0a4d831dd4ecb54779e653b350cad5fdecedf4
Chris Norman <chris.norman2@googlemail.com>
2022-02-20 07:33:53 +0000

    Removed unused constants.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,6 @@
 import 'dart:convert';
 
-import 'package:ziggurat/levels.dart';
-import 'package:ziggurat/menus.dart';
 import 'package:ziggurat/ziggurat.dart';
-
-import 'src/json/zones/zone.dart';
-import 'world_context.dart';
 
 /// The directory where assets will be stored.
 const assetsDirectory = 'assets';
@@ -24,15 +19,6 @@ const defaultDirections = {
 
 /// The type for event callbacks.
 typedef EventCallback<T> = void Function(T event);
-
-/// The type for a function that returns a menu.
-typedef MenuBuilder<T extends Menu> = T Function(WorldContext worldContext);
-
-/// The builder for a menu which takes a zone argument.
-typedef ZoneMenuBuilder<T extends Level> = T Function(
-  WorldContext worldContext,
-  Zone zone,
-);
 
 /// The filename for encrypted world files.
 const encryptedWorldFilename = 'world.data';
