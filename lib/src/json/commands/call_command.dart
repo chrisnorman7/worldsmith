c079f5fe0c82dacd1f04ae7db826cb50d0f7271c
Chris Norman <chris.norman2@googlemail.com>
2022-02-24 22:53:11 +0000

    Added a random chance to a command being run.

--- lib/src/json/commands/call_command.dart
+++ lib/src/json/commands/call_command.dart
@@ -6,7 +6,14 @@ part 'call_command.g.dart';
 @JsonSerializable()
 class CallCommand {
   /// Create an instance.
-  CallCommand({required this.commandId, this.callAfter});
+  CallCommand({
+    required this.commandId,
+    this.callAfter,
+    this.chance = 1,
+  }) : assert(
+          chance >= 1,
+          'The `chance` value must be no less than 1.',
+        );
 
   /// Create an instance from a JSON object.
   factory CallCommand.fromJson(Map<String, dynamic> json) =>
@@ -17,6 +24,12 @@ class CallCommand {
 
   /// How many milliseconds should the game wait before calling the command.
   int? callAfter;
+
+  /// The random number to generate to figure out if this command should run.
+  ///
+  /// If this value is `1`, then the command will always run. Otherwise, there
+  /// will be a 1 in [chance] chance of it running.
+  int chance;
 
   /// Convert an instance to JSON.
   Map<String, dynamic> toJson() => _$CallCommandToJson(this);
--- lib/src/json/commands/call_command.g.dart
+++ lib/src/json/commands/call_command.g.dart
@@ -9,10 +9,12 @@ part of 'call_command.dart';
 CallCommand _$CallCommandFromJson(Map<String, dynamic> json) => CallCommand(
       commandId: json['commandId'] as String,
       callAfter: json['callAfter'] as int?,
+      chance: json['chance'] as int? ?? 1,
     );
 
 Map<String, dynamic> _$CallCommandToJson(CallCommand instance) =>
     <String, dynamic>{
       'commandId': instance.commandId,
       'callAfter': instance.callAfter,
+      'chance': instance.chance,
     };
